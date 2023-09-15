bc2f757b16028b6c0d0e397c954f8230cbc2a6d2
Thomas Schramm <schramm42@gmail.com>
2023-09-15 13:04:57 +0200

    Init

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ const exec = util.promisify(require('node:child_process').exec);
 
 const main = async () => {
     try {
-        // const revList = await getRevList()
-        // const tagList = await getTagList(revList)
         const tagList = await getTagList()
-        //console.log(revList)
-        console.log(tagList)
         const currentTag = tagList.shift()
         const previousTag = tagList.shift()
 
@@ -16,12 +12,13 @@ const main = async () => {
             `currentTag=${currentTag}`,
             `previousTag=${previousTag}`
         ].join('\n')
-
+        
+        console.log(lines)
+        
         if (process.env.GITHUB_OUTPUT) {
             await appendFile(process.env.GITHUB_OUTPUT, `${lines}\n`)
-        } else {
-            console.log(lines)
         }
+
         process.exit(0)
     } catch (err) {
         console.error(err)
@@ -31,36 +28,10 @@ const main = async () => {
 
 async function getTagList() {
     const { fetchStdout, fetchStderr } = await exec(`git fetch --tags -f`)
-    console.log(fetchStdout)
-    console.error(fetchStderr)
     const { stdout, stderr } = await exec(`git tag -l`)
-    console.error(stderr)
     const list = stdout.split('\n').filter((val) => val != "").reverse()
 
     return list
-}
-
-async function getTagListByRevList(revList) {
-    var result = []
-    for (let item of revList) {
-        try {
-            const { stdout, stderr } = await exec(`git describe --abbrev=0 --tags ${item}`)
-            result.push(stdout.trim())
-        } catch (err) {
-            if (!err.stderr.includes("No tags can describe")) {
-                throw err
-            }
-        }
-    }
-
-    return result
-}
-
-async function getRevList() {
-    const { stdout, stderr } = await exec(`git rev-list --tags`)
-    const revList = stdout.split('\n').filter((val) => val != "")
-
-    return revList
 }
 
 main()

8fbe04f2434b0528b889c056532890dd488565da
Thomas Schramm <schramm42@gmail.com>
2023-09-15 12:04:39 +0200

    Init

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
-const { exec } = require('child_process');
-const fs = require('fs');
+const { appendFile } = require('node:fs/promises');
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
 
-exec(`git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1)`, (err, tag, stderr) => {
-    tag = tag.trim();
+const main = async () => {
+    try {
+        const revList = await getRevList()
+        const tagList = await getTagList(revList)
+        const currentTag = tagList.shift()
+        const previousTag = tagList.shift()
 
-    if (err) {
-        console.error('Could not find any tags because: ');
-        console.error(stderr);
-        process.exit(1);
-    } 
-    
-    if (tag === "") {
-        console.log(`Use default tag: ${process.env.INPUT_FALLBACK}`);
-        fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${process.env.INPUT_FALLBACK}\n`);
-        process.exit(0);
+        const lines = [
+            `currentTag=${currentTag}`,
+            `previousTag=${previousTag}`
+        ].join('\n')
+
+        if (process.env.GITHUB_OUTPUT) {
+            await appendFile(process.env.GITHUB_OUTPUT, `${lines}\n`)
+        } else {
+            console.log(lines)
+        }
+        process.exit(0)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
+}
+
+async function getTagList(revList) {
+    var result = []
+    for (let item of revList) {
+        try {
+            const { stdout, stderr } = await exec(`git describe --abbrev=0 --tags ${item}`)
+            result.push(stdout.trim())
+        } catch (err) {
+            if (!err.stderr.includes("No tags can describe")) {
+                throw err
+            }
+        }
     }
 
-    console.log(`Found tag: ${tag}`);
-    fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${tag}\n`);
-    process.exit(0);
+    return result
+}
 
-});
+async function getRevList() {
+    const { stdout, stderr } = await exec(`git rev-list --tags`)
+    const revList = stdout.split('\n').filter((val) => val != "")
+
+    return revList
+}
+
+main()
